d741d5055df6db77e349d71fd496efa70d1b0af8
chinmay <chinu1400@gmail.com>
2020-02-09 18:31:36 +0530

    travis ci

--- lib/main.dart
+++ lib/main.dart
@@ -34,7 +34,9 @@ class _MyHomePageState extends State<MyHomePage> {
         // the App.build method, and use it to set our appbar title.
         title: Text(widget.title),
       ),
-      body: Center(),
+      body: Center(
+        child: Text('Hello World'),
+      ),
     );
   }
 }
